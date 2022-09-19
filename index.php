4d51926d8ed59b0ae35ed353fbadf5030bf04887
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-09-19 11:54:50 -0500

    post change

--- index.php
+++ index.php
@@ -4,26 +4,11 @@
 
 <h1>My first PHP page</h1>
 
-<?php
-echo "Hello World!";
-?> 
-
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fname">
-  <input type="submit">
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
 </form>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // collect value of input field
-  $name = $_POST['fname']; 
-  if (empty($name)) {
-    echo "Name is empty";
-  } else {
-    echo $name;
-  }
-}
-?>
 
 
 </body>
