24f078980db8e917be9a3e13ebd173eefd81269d
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-09-19 12:01:51 -0500

    Adding get

--- index.php
+++ index.php
@@ -4,7 +4,15 @@
 
 <h1>My first PHP page</h1>
 
+<p>Using POST:</p>
 <form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+<p>Using GET:</p>
+<form action="welcome_get.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
